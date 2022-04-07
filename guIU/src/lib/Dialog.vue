323b3a49678c1d634367e39026f80d27287541c3
xiaocao <853044263@qq.com>
2022-04-07 22:14:15 +0800

    Dialog组件初步完成

---
+++ guIU/src/lib/Dialog.vue
@@ -0,0 +1,131 @@
+<template>
+  <template v-if="visible">
+    <Teleport to="body">
+      <div class="guIu-dialog-overlay" @click="noclose"></div>
+      <div class="guIu-dialog-wrapper">
+        <div class="guIu-dialog">
+          <header>
+            <slot name="title"/>
+            <span class="guIu-dialog-close" @click="close"></span>
+          </header>
+          <main>
+            <slot name="content" />
+          </main>
+          <footer>
+            <Button level="main" @click="ok">OK</Button>
+            <Button @click="Cancel">Cancel</Button>
+          </footer>
+        </div>
+      </div>
+    </Teleport>
+  </template>
+</template>
+<script lang="ts">
+import Button from "./Button.vue";
+export default {
+  components: { Button },
+  props: {
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+    noclose: {
+      type: Boolean,
+      default: true,
+    },
+    ok: {
+      type: Function,
+    },
+    Cancel: {
+      type: Function,
+    },
+  },
+  setup(props, context) {
+    const close = () => {
+      context.emit("update:visible", false);
+    };
+    const noclose = () => {
+      if (props.noclose) {
+        close();
+      }
+    };
+    const ok = () => {
+      if (props.ok?.() !== false) {
+        close();
+      }
+    };
+    const Cancel = () => {
+      context.emit("cancel");
+      close();
+    };
+
+    return { close, noclose, ok, Cancel };
+  },
+};
+</script>
+<style lang="scss">
+$radius: 4px;
+$border-color: #d9d9d9;
+.guIu-dialog {
+  background: white;
+  border-radius: $radius;
+  box-shadow: 0 0 3px fade_out(black, 0.5);
+  min-width: 15em;
+  max-width: 90%;
+  &-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: fade_out(black, 0.5);
+    z-index: 11;
+  }
+  &-wrapper {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 12;
+  }
+  > header {
+    padding: 12px 16px;
+    border-bottom: 1px solid $border-color;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 20px;
+  }
+  > main {
+    padding: 12px 16px;
+  }
+  > footer {
+    padding: 12px 16px;
+    border-top: 1px solid $border-color;
+    text-align: right;
+  }
+  &-close {
+    position: relative;
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    cursor: pointer;
+    &::before,
+    &::after {
+      content: "";
+      position: absolute;
+      height: 1px;
+      background: black;
+      width: 100%;
+      top: 50%;
+      left: 50%;
+    }
+    &::before {
+      transform: translate(-50%, -50%) rotate(-45deg);
+    }
+    &::after {
+      transform: translate(-50%, -50%) rotate(45deg);
+    }
+  }
+}
+</style>
