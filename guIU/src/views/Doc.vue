62c6e8e6cc2ff25d5fa352f49d5f145d892eb231
xiaocao <853044263@qq.com>
2022-04-08 20:13:33 +0800

    l路由高亮

--- guIU/src/views/Doc.vue
+++ guIU/src/views/Doc.vue
@@ -3,6 +3,18 @@
     <Topanv class="nav" toggleMenuButtonVisible />
     <div class="content">
       <aside v-if="menuVisible">
+        <h2>文档</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/intro">介绍</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/get-started">安装</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/install">开始使用</router-link>
+          </li>
+        </ol>
         <h2>组件列表</h2>
         <ol>
           <li>
@@ -26,66 +38,74 @@
   </div>
 </template>
 <script lang="ts">
-import { inject, Ref } from 'vue';
-import Topanv from '../components/Topanv.vue'
+import { inject, Ref } from "vue";
+import Topanv from "../components/Topanv.vue";
 export default {
   components: { Topanv },
   setup() {
-   const menuVisible = inject<Ref<boolean>>('menuVisible')
-   return {menuVisible}
-  }
+    const menuVisible = inject<Ref<boolean>>("menuVisible");
+    return { menuVisible };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.layout{
+.layout {
   display: flex;
   flex-direction: column;
   height: 100vh;
-  > .nav{
-    flex-shrink: 0
+  > .nav {
+    flex-shrink: 0;
   }
-  >.content{
+  > .content {
     flex-grow: 1;
     padding-top: 60px;
     padding-left: 156px;
-    @media (max-width:500px) {
-      padding-left:0;
+    @media (max-width: 500px) {
+      padding-left: 0;
     }
   }
 }
-.content{
+.content {
   display: flex;
-  >aside{
+  > aside {
     flex-shrink: 0;
   }
-  >main{
+  > main {
     flex-grow: 1;
     padding: 16px;
     background: white;
   }
 }
 
-aside{
+aside {
   background: lightblue;
   width: 150px;
-  padding: 16px;
+  padding: 16px 0;
   position: fixed;
-  top:0;
+  top: 0;
   left: 0;
   padding-top: 70px;
-  height:100%;
+  height: 100%;
   z-index: 9;
-  >h2{
+  > h2 {
     margin-bottom: 4px;
+    padding: 4px 16px;
   }
-  > ol{
-    >li{
-      padding: 4px 0; 
+  > ol {
+    > li {
+      > a {
+        display: block;
+        padding: 4px 16px;
+        text-decoration: none;
+      }
+      .router-link-active {
+        background: white;
+      }
     }
   }
 }
-main{
+main {
   overflow: auto;
 }
 </style>
