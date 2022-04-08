c2ed3eb5a923de411b135b951043d825e1d6ab8a
xiaocao <853044263@qq.com>
2022-04-08 09:13:31 +0800

    更新

--- guIU/src/components/TabsDemo.vue
+++ guIU/src/components/TabsDemo.vue
@@ -1,9 +1,16 @@
 <template>
-    <div>TabsDemo</div>
+    <div>Tabs组件</div>
+    <h1>实例1</h1>
+    <tabs>
+        <tab title="nei1">内容1</tab>
+        <tab title="nei2">内容1</tab>
+    </tabs>
 </template>
 <script lang="ts">
-
+import Tabs from '../lib/Tabs.vue'
+import Tab from '../lib/Tab.vue'
 export default{
+    components:{Tabs,Tab},
     setup() {
         
     },
