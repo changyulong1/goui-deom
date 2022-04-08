65390ae7f2eba8cf2f783539b2d7a39cb1c75427
xiaocao <853044263@qq.com>
2022-04-08 10:55:58 +0800

    vue3的bug

--- guIU/src/components/TabsDemo.vue
+++ guIU/src/components/TabsDemo.vue
@@ -1,18 +1,20 @@
 <template>
     <div>Tabs组件</div>
     <h1>实例1</h1>
-    <tabs>
-        <tab title="nei1">内容1</tab>
-        <tab title="nei2">内容1</tab>
+    <tabs v-model:selected="x">
+        <Tab title="导航1">内容1</Tab>
+        <Tab title="导航2">内容1</Tab>
     </tabs>
 </template>
 <script lang="ts">
 import Tabs from '../lib/Tabs.vue'
 import Tab from '../lib/Tab.vue'
+import { ref } from 'vue'
 export default{
     components:{Tabs,Tab},
     setup() {
-        
+        const x = ref('导航1')
+        return {x}
     },
 }
 </script>
--- guIU/src/lib/Tabs.vue
+++ guIU/src/lib/Tabs.vue
@@ -1,5 +1,82 @@
 <template>
-    <div>
-        tabs组件
+  <div class="guIu-tabs">
+    <div class="guIu-tabs-nav">
+      <div
+        class="guIu-tabs-nav-item"
+        v-for="(t, index) in titles"
+        @click="select(t)"
+        :class="{ selected: t === selected }"
+        :key="index"
+      >
+        {{ t }}
+      </div>
     </div>
+    <div class="guIu-tabs-content">
+        {{ cunt }}
+      <component class="guIu-tabs-content-item" :is="cunt"></component>
+    </div>
+  </div>
 </template>
+<script lang="ts">
+import { computed } from "vue";
+import Tab from "./Tab.vue";
+export default {
+  props: {
+    selected: {
+      type: String,
+    },
+  },
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error("Tabs内部标签必须是Tab");
+      }
+    });
+    const cunt = computed(() => {
+      return defaults.filter((tag) => {
+        return tag.props.title === props.selected;
+      })[0];
+    });
+    const titles = defaults.map((tag) => {
+      return tag.props.title;
+    });
+    const select = (title: String) => {
+      context.emit("update:selected", title);
+    };
+    return {
+      defaults,
+      titles,
+      cunt,
+      select,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.guIu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
