d7808518742090f904053fb475dd7da759cfb6a1
xiaocao <853044263@qq.com>
2022-04-09 19:00:08 +0800

    更新

--- guIU/src/lib/Switch.vue
+++ guIU/src/lib/Switch.vue
@@ -1,62 +1,62 @@
 <template>
-  <!-- <button @click="toggle" class="guIu-Switch" :class="{ 'guIu-checked': value }">
+  <button @click="toggle" class="guIu-Switch" :class="{ 'guIu-checked': value }">
     <span></span>
-  </button> -->
+  </button>
 </template>
 <script lang="ts">
-// export default {
-//   props: {
-//     value: {
-//       type: Boolean,
-//     },
-//   },
-//   setup(props, context) {
-//     const toggle = () => {
-//       context.emit("update:value", !props.value);
-//     };
-//     return { toggle };
-//   },
-// };
+export default {
+  props: {
+    value: {
+      type: Boolean,
+    },
+  },
+  setup(props, context) {
+    const toggle = () => {
+      context.emit("update:value", !props.value);
+    };
+    return { toggle };
+  },
+};
 </script>
 <style lang="scss">
-// $h: 22px;
-// $h1: $h - 4px;
-// .guIu-Switch {
-//   height: $h;
-//   width: $h * 2;
-//   border-radius: calc($h/2);
-//   border: none;
-//   background: #bfbfbf;
-//   position: relative;
-//   > span {
-//     position: absolute;
-//     top: 2px;
-//     left: 2px;
-//     width: $h1;
-//     height: $h1;
-//     background: white;
-//     border-radius: calc($h/2);
-//     transition: all 250ms;
-//   }
-//   &.guIu-checked {
-//     background: #1890ff;
-//     > span {
-//       left: calc(100% - #{$h1} - 2px);
-//     }
-//   }
-//   &:focus {
-//     outline: none;
-//   }
-//   &:active {
-//     > span {
-//       width: $h1 + 4px;
-//     }
-//   }
-//   &.guIu-checked:active {
-//     > span {
-//       width: $h1 + 4px;
-//       margin-left: -4px;
-//     }
-//   }
-// }
+$h: 22px;
+$h1: $h - 4px;
+.guIu-Switch {
+  height: $h;
+  width: $h * 2;
+  border-radius: calc($h/2);
+  border: none;
+  background: #bfbfbf;
+  position: relative;
+  > span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    width: $h1;
+    height: $h1;
+    background: white;
+    border-radius: calc($h/2);
+    transition: all 250ms;
+  }
+  &.guIu-checked {
+    background: #1890ff;
+    > span {
+      left: calc(100% - #{$h1} - 2px);
+    }
+  }
+  &:focus {
+    outline: none;
+  }
+  &:active {
+    > span {
+      width: $h1 + 4px;
+    }
+  }
+  &.guIu-checked:active {
+    > span {
+      width: $h1 + 4px;
+      margin-left: -4px;
+    }
+  }
+}
 </style>
