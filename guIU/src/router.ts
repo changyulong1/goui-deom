9d2bdef03448ab827091ec0827e22e33c48d0f78
xiaocao <853044263@qq.com>
2022-04-09 08:48:03 +0800

    代码优化

--- guIU/src/router.ts
+++ guIU/src/router.ts
@@ -9,6 +9,8 @@ import Intro from './views/Intro.vue'
 import GetStarted from './views/GetStarted.vue'
 import Install from './views/Install.vue'
 import {createWebHashHistory,createRouter} from 'vue-router'
+import marked from './components/marked.vue'
+import { h } from 'vue'
 
 const history = createWebHashHistory()
 
@@ -21,9 +23,9 @@ export const router = createRouter({
             component:Doc,
             children:[
                 {path:'',component:DocDemo},
-                {path:'intro',component:Intro},
-                {path:'get-started',component:GetStarted},
-                { path: "install", component: Install },
+                {path:'intro',component:h(marked,{path:'../markdown/intro.md',key:1})},
+                {path:'get-started',component:h(marked,{path:'../markdown/get-started.md',key:2})},
+                { path: "install", component: h(marked,{path:'../markdown/install.md',key:3}) },
                 {path:'switch',component:SwitchDemo},
                 {path:'button',component:ButtonDemo},
                 {path:'dialog',component:DialogDemo},
