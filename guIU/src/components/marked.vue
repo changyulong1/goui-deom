f4d99b97bf2da4f06dc64cf8cff2779a28d6cc4d
xiaocao <853044263@qq.com>
2022-04-09 08:39:18 +0800

    md组件代码优化

---
+++ guIU/src/components/marked.vue
@@ -0,0 +1,22 @@
+<template>
+  <article class="markdown-body" v-html="content">
+  </article>
+</template>
+<script>
+import { ref } from 'vue'
+export default {
+    props:{
+        path:{
+            tupe:String,
+            required: true
+        }
+    },
+   setup(props){
+       const content = ref(null)
+       import(props.path).then((res)=>{
+           content.value = res.default
+       })
+        return {content}
+   }
+}
+</script>
